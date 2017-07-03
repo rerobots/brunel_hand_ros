01960ca6e337325acd83e0688b813566f2178a73
Scott C. Livingston <scott@rerobots.net>
2017-07-03 15:51:36 -0400

    Add ROS message for finger positions (raw)

--- src/diagnostics.cpp
+++ src/diagnostics.cpp
@@ -15,15 +15,19 @@
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 #include "ros/ros.h"
 #include "serial/serial.h"
+
+#include "brunel_hand_ros/FingerPose.h"
 
 
 int main( int argc, char **argv )
 {
     ros::init( argc, argv, "bhand_diagnostics" );
     ros::NodeHandle nh;
+    ros::Publisher posep;
 
     std::string devfile;
     nh.param( "hand_dev_file", devfile, std::string( "/dev/ttyACM0" ) );
@@ -36,9 +40,38 @@ int main( int argc, char **argv )
         return 1;
     }
 
+    posep = nh.advertise<brunel_hand_ros::FingerPose>( "fpose", 10, true );
 
-    ros::Rate rate( 1. );
+    ros::Rate rate( 100 );
     while (ros::ok()) {
+        std::string line = bhandcom.readline();
+
+        // If first item is not digit, then assume that this messages is not the
+        // CSV finger data message type.
+        if (line[0] != '0' && line[0] != '1' && line[0] != '2' && line[0] != '3'
+            && line[0] != '4' && line[0] != '5' && line[0] != '6'
+            && line[0] != '7' && line[0] != '8' && line[0] != '9')
+            continue;
+
+        brunel_hand_ros::FingerPose currentpose;
+        currentpose.header.stamp = ros::Time::now();
+        int thispos = -1;  // -1 => first search is with respect to position 0
+        int nextpos = 0;
+        std::string numstring;
+        while ((nextpos = line.find( "," , thispos+1))
+               != std::string::npos) {
+            numstring = line.substr( thispos+1, nextpos-thispos-1 );
+            thispos = nextpos;
+            currentpose.f.push_back( std::atoi( numstring.c_str() ) );
+        }
+        numstring = line.substr( thispos+1 );
+        if (numstring[0] != ' '
+            && numstring[0] != '\r'
+            &&  numstring[0] != '\n')
+            currentpose.f.push_back( std::atoi( numstring.c_str() ) );
+
+        posep.publish( currentpose );
+
         ros::spinOnce();
         rate.sleep();
     }
