fd91faa48d67b54acbbf51913185d5433caad30c
Scott C. Livingston <scott@rerobots.net>
2017-07-17 23:59:13 -0700

    Change node name to indicate that it is proxy

--- src/proxy.cpp
+++ src/proxy.cpp
@@ -25,7 +25,7 @@
 
 int main( int argc, char **argv )
 {
-    ros::init( argc, argv, "bhand_diagnostics" );
+    ros::init( argc, argv, "brunel_hand_proxy" );
     ros::NodeHandle nh;
     ros::Publisher posep;
 
